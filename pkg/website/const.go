e2597f19b9fe0e3e967a946cd2a957052e608b39
thomas-senechal <thomas.senechal@pm.me>
2024-07-23 20:41:58 +0200

    Add Upload website func and some website constants

---
+++ pkg/website/const.go
@@ -0,0 +1,8 @@
+package website
+
+const (
+	nbChunkKey = "\000"
+	ownerKey   = "OWNER"
+
+	ChunkSize = 64_000 // 64KB
+)
---
+++ pkg/website/upload.go
@@ -0,0 +1,83 @@
+package website
+
+import (
+	"fmt"
+
+	"github.com/massalabs/DeWeb/int/config"
+	mwUtils "github.com/massalabs/station-massa-wallet/pkg/utils"
+	"github.com/massalabs/station/pkg/convert"
+	"github.com/massalabs/station/pkg/logger"
+	"github.com/massalabs/station/pkg/node/sendoperation"
+	"github.com/massalabs/station/pkg/node/sendoperation/signer"
+	"github.com/massalabs/station/pkg/onchain"
+)
+
+const (
+	appendFunction = "appendBytesToWebsite"
+)
+
+func UploadChunk(
+	websiteAddress string,
+	config *config.Config,
+	chunk []byte,
+	chunkIndex int,
+) (operationID string, err error) {
+	if !mwUtils.IsValidAddress(websiteAddress) {
+		return "", fmt.Errorf("invalid website address")
+	}
+
+	if len(chunk) == 0 {
+		return "", fmt.Errorf("chunk is empty, no data to upload")
+	}
+
+	params, err := prepareUploadParams(chunk, chunkIndex)
+	if err != nil {
+		return "", fmt.Errorf("preparing upload params: %w", err)
+	}
+
+	uploadCost, err := ComputeChunkCost(chunkIndex, len(chunk))
+	if err != nil {
+		return "", fmt.Errorf("computing chunk cost: %w", err)
+	}
+
+	logger.Debugf("Uploading chunk %d to website at address %s with %d nMAS", chunkIndex, websiteAddress, uploadCost)
+
+	return performUpload(config, websiteAddress, params, uploadCost, chunkIndex)
+}
+
+func prepareUploadParams(chunk []byte, chunkIndex int) ([]byte, error) {
+	params := convert.I32ToBytes(chunkIndex)
+	params = append(params, convert.U32ToBytes(len(chunk))...)
+	params = append(params, chunk...)
+
+	return params, nil
+}
+
+func performUpload(
+	config *config.Config,
+	websiteAddress string,
+	params []byte,
+	uploadCost int,
+	chunkIndex int,
+) (string, error) {
+	res, err := onchain.CallFunction(
+		&config.NetworkInfos,
+		config.WalletNickname,
+		websiteAddress,
+		appendFunction,
+		params,
+		config.MinimalFees,
+		config.MaxGas,
+		uint64(uploadCost),
+		config.Expiry,
+		false,
+		sendoperation.OperationBatch{},
+		&signer.WalletPlugin{},
+		fmt.Sprintf("Uploading chunk %d", chunkIndex),
+	)
+	if err != nil {
+		return "", fmt.Errorf("uploading chunk %d failed: %w", chunkIndex, err)
+	}
+
+	return res.OperationResponse.OperationID, nil
+}
