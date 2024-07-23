4419e8f8a44c2805ed12933dedbcabbe822b2a1c
Phoebe Lartisant <phoebelartisant@proton.me>
2024-07-23 14:48:23 +0200

    add readZipContent fn

---
+++ int/zipper/zipper.go
@@ -0,0 +1,23 @@
+package zip
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+)
+
+
+func GetZipFileContent(z *zip.File) ([]byte, error) {
+file, err := z.Open()
+	if err != nil {
+		return nil, fmt.Errorf("opening zip content: %w", err)
+	}
+	defer file.Close()
+
+	content, err := io.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("reading zip content: %w", err)
+	}
+
+	return content, nil
+}
