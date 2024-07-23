a6e338426472f554d7780e4522d7cde097dcc235
Phoebe Lartisant <phoebelartisant@proton.me>
2024-07-23 17:10:27 +0200

    add tests

--- pkg/website/chunk.go
+++ pkg/website/chunk.go
@@ -1,17 +1,26 @@
 package chunk
 
-func DivideIntoChunks(data []byte, chunkSize int) [][]byte {
-    if data == nil || chunkSize <= 0 {
-        return nil
-    }
+import "fmt"
 
-    var chunks [][]byte
-    for i := 0; i < len(data); i += chunkSize {
-        end := i + chunkSize
-        if end > len(data) {
-            end = len(data)
-        }
-        chunks = append(chunks, data[i:end])
-    }
-    return chunks
+func DivideIntoChunks(data []byte, chunkSize int) [][]byte {
+	if data == nil || chunkSize <= 0 {
+		return nil
+	}
+
+	var chunks [][]byte
+
+	fmt.Printf("Data: %v\nLength: %v\n", data, len(data))
+
+	for i := 0; i < len(data); i += chunkSize {
+		end := i + chunkSize
+		if end > len(data) {
+			end = len(data)
+		}
+
+		chunks = append(chunks, data[i:end])
+	}
+
+	return chunks
 }
+
+// string => byte arr ==> function ==> assert chunk length
