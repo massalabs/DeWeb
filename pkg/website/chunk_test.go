00803fef59861425d12e0de316ba1787fc26cd6e
thomas-senechal <thomas.senechal@pm.me>
2024-07-25 09:15:14 +0200

    Improve Chunk tests and add some test cases

--- pkg/website/chunk_test.go
+++ pkg/website/chunk_test.go
@@ -1,6 +1,7 @@
 package website
 
 import (
+	_ "embed"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,6 +19,10 @@ func TestChunk(t *testing.T) {
 		{"Median", []byte("Hello, World!"), 4},
 		{"Big byte array", []byte("Lorem ipsum dolor sit amet, consectetur adipiscing elit."), 16},
 		{"Small byte array", []byte("hello"), 32},
+		{"Single character", []byte("a"), 2},
+		{"Exact divisible length", []byte("123456"), 2},
+		{"Long sentence", []byte("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."), 3},
+		{"Website deployer wasm", sc, ChunkSize},
 	}
 
 	for _, test := range tests {
@@ -26,13 +31,27 @@ func TestChunk(t *testing.T) {
 
 			if test.data == nil || test.chunkSize <= 0 {
 				assert.Nil(t, chunks)
-
 				return
 			}
 
 			expectedLen := (len(test.data) + test.chunkSize - 1) / test.chunkSize
-
 			assert.Equal(t, len(chunks), expectedLen)
+
+			expectedLastChunkLen := len(test.data) % test.chunkSize
+			if expectedLastChunkLen == 0 && len(test.data) > 0 {
+				expectedLastChunkLen = test.chunkSize
+			}
+
+			// Assert the length of each chunk except the last one
+			for i := 0; i < len(chunks)-1; i++ {
+				assert.Equal(t, test.chunkSize, len(chunks[i]), "Chunk size does not match the expected value.")
+			}
+
+			// Assert the length of the last chunk
+			if len(chunks) > 0 {
+				lastChunk := chunks[len(chunks)-1]
+				assert.Equal(t, expectedLastChunkLen, len(lastChunk), "The length of the last chunk does not match the expected value.")
+			}
 		})
 	}
 }
