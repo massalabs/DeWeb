4ebf48eb1e5db6210e621ec69193ad98c0678c32
Phoebe Lartisant <phoebelartisant@proton.me>
2024-07-30 16:29:57 +0200

    add time comparison

--- pkg/websiteManager/websiteManager.go
+++ pkg/websiteManager/websiteManager.go
@@ -2,6 +2,7 @@ package websiteManager
 
 import (
 	"fmt"
+	"time"
 
 	pkgConfig "github.com/massalabs/DeWeb/pkg/config"
 	"github.com/massalabs/DeWeb/pkg/website"
@@ -14,12 +15,20 @@ func RequestWebsite(scAddress string, config *pkgConfig.Config) ([]byte, error) 
 	cache := new(cache.Cache)
 	fileName := fmt.Sprintf("website_%s.zip", scAddress)
 
-	if cache.IsPresent(fileName) {
-		logger.Infof("Website found in cache at: %s", fileName)
+	// fake last updated date
+	lastUpdated := time.Now()
+
+	// fake creation date
+	creationDate := time.Now()
+
+	shouldFetchWebsite := shouldFetch(fileName, cache, lastUpdated, creationDate)
+
+	if shouldFetchWebsite {
 		content, err := cache.Read(fileName)
 		if err != nil {
 			return nil, fmt.Errorf("failed to read cached website: %w", err)
 		}
+
 		return content, nil
 	}
 
@@ -37,5 +46,18 @@ func RequestWebsite(scAddress string, config *pkgConfig.Config) ([]byte, error) 
 	}
 
 	logger.Infof("Website successfully written to cache at: %s", fileName)
+
 	return websiteBytes, nil
+}
+
+// if the lastUpdated timestamp changes then the website should be fetched again
+func shouldFetch(fileName string, cache *cache.Cache, lastUpdated time.Time, creationDate time.Time) bool {
+	if cache.IsPresent(fileName) && creationDate == lastUpdated {
+		logger.Infof("Website found in cache at: %s", fileName)
+
+		return true
+
+	}
+
+	return false
 }
