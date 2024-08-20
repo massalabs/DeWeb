3e53f19cbccc8a00fb9703ee0391976d183f0856
thomas-senechal <thomas.senechal@pm.me>
2024-08-20 17:12:49 +0200

    Add localHandler function to serve local pages

---
+++ int/api/local.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/massalabs/DeWeb/int/zipper"
+	"github.com/massalabs/station/pkg/logger"
+)
+
+func localHandler(w http.ResponseWriter, zipBytes []byte, resourceName string) {
+	if resourceName == "" {
+		logger.Debugf("localHandler: No resource specified, using index.html")
+		resourceName = "index.html"
+	}
+
+	logger.Debugf("localHandler: %s", resourceName)
+
+	content, err := zipper.ReadFileFromZip(zipBytes, resourceName)
+	if err != nil {
+		logger.Warnf("Filenot found: %b", zipper.IsNotFoundError(err, resourceName))
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	contentType := ContentType(resourceName, content)
+	logger.Debugf("localHandler: %s, %s", resourceName, contentType)
+
+	w.Header().Set("Content-Type", contentType)
+	w.WriteHeader(http.StatusOK)
+	w.Write(content)
+}
