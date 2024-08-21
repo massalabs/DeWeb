1d5066b4e5a28b9e6b159d8249e6779ea72b434a
thomas-senechal <thomas.senechal@pm.me>
2024-08-21 09:50:21 +0200

    Fix windows zip and mv errors

--- pages/build.mjs
+++ pages/build.mjs
@@ -1,4 +1,6 @@
 import { execSync } from "child_process";
+import fs from "fs";
+import archiver from "archiver";
 
 // List of pages to build
 const pages = ["brokenWebsite", "domainNotFound", "home", "notAvailable"];
@@ -14,8 +16,22 @@ for (const page of pages) {
   });
 
   console.log(`Zipping ${page}.html...`);
-  execSync(`zip -r ../../${page}.zip ./*`, {
-    stdio: "inherit",
-    cwd: `dist/${page}`,
+  const output = fs.createWriteStream(`./${page}.zip`);
+  const archive = archiver('zip', {
+    zlib: { level: 9 } // Sets the compression level.
   });
+
+  output.on('close', function() {
+    console.log(`${page}.zip has been created and contains ${archive.pointer()} total bytes`);
+  });
+
+  archive.on('error', function(err) {
+    throw err;
+  });
+
+  archive.pipe(output);
+
+  archive.directory(`dist/${page}/`, false);
+
+  archive.finalize();
 }
