233d010d756e48b07046b7636f5f3fe240c6819b
thomas-senechal <thomas.senechal@pm.me>
2024-09-18 23:29:48 +0200

    Update eslint to v9

---
+++ cli/eslint.config.mjs
@@ -0,0 +1,34 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+import tsParser from '@typescript-eslint/parser'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default [
+  {
+    ignores: ['**/bin', '**/node_modules', '**/scripts', 'jest.config.ts'],
+  },
+  ...compat.extends('@massalabs', 'prettier'),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    rules: {
+      'tsdoc/syntax': 'warn',
+      'max-len': ['error', 200],
+      camelcase: 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'no-console': 'off',
+      'comma-dangle': 'off',
+    },
+  },
+]
