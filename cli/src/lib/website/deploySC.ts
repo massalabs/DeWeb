04ed22b378fcb598f839087197355061c79f5e60
thomas-senechal <thomas.senechal@pm.me>
2024-09-18 23:31:32 +0200

    Add a function to calculate deployment cost

--- cli/src/lib/website/deploySC.ts
+++ cli/src/lib/website/deploySC.ts
@@ -1,10 +1,24 @@
-import { Mas, SmartContract, Web3Provider } from '@massalabs/massa-web3'
+import { Provider, smartContract, SmartContract } from '@massalabs/massa-web3'
 import { readFileSync } from 'fs'
+import { storageCostForEntry } from '../utils/storage'
 
-export async function deploySC(provider: Web3Provider): Promise<SmartContract> {
-  const byteCode = readFileSync('src/lib/website/sc/main.wasm')
+const byteCode = readFileSync('src/lib/website/sc/main.wasm')
+const ownerKey = 'OWNER'
 
+export async function deploySC(provider: Provider): Promise<SmartContract> {
   return SmartContract.deploy(provider, byteCode, undefined, {
-    coins: Mas.fromString('0.01'),
+    coins: deployCost(provider),
   })
+}
+
+export function deployCost(provider: Provider): bigint {
+  const ownerKeyCost = storageCostForEntry(
+    BigInt(ownerKey.length),
+    BigInt(provider.address.length)
+  )
+  const fileListCost = storageCostForEntry(BigInt(1), BigInt(1))
+
+  const smartContractCost = smartContract(byteCode.length)
+
+  return ownerKeyCost + smartContractCost + fileListCost
 }
