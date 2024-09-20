6ad5c486a1c0912637808c3182291a9201cfd4ee
thomas-senechal <thomas.senechal@pm.me>
2024-09-20 16:58:42 +0200

    Fix entry storage cost calculation

--- cli/src/lib/utils/storage.ts
+++ cli/src/lib/utils/storage.ts
@@ -1,7 +1,7 @@
 import { Mas } from '@massalabs/massa-web3'
 
 const STORAGE_BYTE_COST = Mas.fromString('0.0001')
-const NEW_LEDGER_ENTRY_COST = STORAGE_BYTE_COST * 4n
+const NEW_STORAGE_ENTRY_COST = 4n
 
 /**
  * Compute the storage cost for a given key and value size based on the documentation at:
@@ -14,5 +14,5 @@ export function storageCostForEntry(
   keySize: bigint,
   valueSize: bigint
 ): bigint {
-  return (keySize + valueSize + NEW_LEDGER_ENTRY_COST) * STORAGE_BYTE_COST
+  return (keySize + valueSize + NEW_STORAGE_ENTRY_COST) * STORAGE_BYTE_COST
 }
