739d10dcee13297233e9cbfa862b94c7f1d9b1c7
Phoebe Lartisant <phoebelartisant@proton.me>
2024-09-26 11:19:55 +0200

    add e2e test

--- smart-contract/src/e2e/delete.ts
+++ smart-contract/src/e2e/delete.ts
@@ -1,7 +1,23 @@
 /* eslint-disable no-console */
-import { SmartContract } from '@massalabs/massa-web3';
-import { assertFileIsDeleted, assertListIsEmpty, deleteFiles } from './helpers';
-import { CSS_FILE, HTML_FILE, JS_FILE } from './helpers/const';
+import { Args, SmartContract } from '@massalabs/massa-web3';
+import {
+  assertFileIsDeleted,
+  assertListIsEmpty,
+  deleteFiles,
+  preStoreChunks,
+  storeChunk,
+} from './helpers';
+import {
+  CHUNK_LIMIT,
+  CSS_FILE,
+  HTML_FILE,
+  JS_FILE,
+  projectPath,
+} from './helpers/const';
+import { PreStore } from './helpers/serializable/Chunk';
+import { sha256 } from 'js-sha256';
+import { generateChunk } from './store';
+import { getByteCode } from '../utils';
 
 export async function _deleteFile(contract: SmartContract) {
   console.log('Deleting 1 file');
@@ -18,4 +34,43 @@ export async function _deleteFiles(contract: SmartContract) {
 export async function testDeleteFiles(contract: SmartContract) {
   await _deleteFile(contract);
   await _deleteFiles(contract);
+}
+
+export async function testDeleteWebsite(contract: SmartContract) {
+  console.log('Deleting website...');
+  const op = await contract.call('deleteWebsite', new Args().serialize());
+  await op.waitSpeculativeExecution();
+
+  await assertListIsEmpty(contract);
+}
+
+export async function generateWebsite(contract: SmartContract) {
+  const htmlPreStore = new PreStore(
+    HTML_FILE,
+    new Uint8Array(sha256.arrayBuffer(HTML_FILE)),
+    1n,
+  );
+
+  const cssPreStore = new PreStore(
+    CSS_FILE,
+    new Uint8Array(sha256.arrayBuffer(CSS_FILE)),
+    1n,
+  );
+
+  await preStoreChunks(contract, [htmlPreStore, cssPreStore]);
+
+  const chunkHtml = generateChunk(
+    HTML_FILE,
+    getByteCode(projectPath, HTML_FILE),
+    0n,
+  );
+
+  const chunkCss = generateChunk(
+    CSS_FILE,
+    getByteCode(`${projectPath}/assets`, CSS_FILE),
+    0n,
+  );
+
+  await storeChunk(contract, [chunkHtml]);
+  await storeChunk(contract, [chunkCss]);
 }
--- smart-contract/src/e2e/index.ts
+++ smart-contract/src/e2e/index.ts
@@ -5,7 +5,7 @@ import {
   SmartContract,
   Web3Provider,
 } from '@massalabs/massa-web3';
-import { testDeleteFiles } from './delete';
+import { generateWebsite, testDeleteFiles, testDeleteWebsite } from './delete';
 import { testStoreFiles } from './store';
 import { getByteCode } from '../utils';
 import { CONTRACT_FILE } from './helpers/const';
@@ -21,17 +21,26 @@ async function deploy(provider: Web3Provider): Promise<SmartContract> {
   return contract;
 }
 
-async function main() {
-  console.log('Starting tests...');
+async function generateContract(): Promise<SmartContract> {
+  console.log('Starting first tests...');
   const account = await Account.fromEnv();
   const provider = Web3Provider.buildnet(account);
   const contract = await deploy(provider);
+  return contract;
+}
 
+async function main() {
+  const contract = await generateContract();
   console.log('Testing store files...');
   await testStoreFiles(contract);
-  console.log('Testing deletion...');
+  console.log('Finished test\n');
+  console.log('Testing file deletion...');
   await testDeleteFiles(contract);
-  console.log('Tests finished');
+  console.log('Finished test\n');
+  console.log('Testing website deletion...');
+  await generateWebsite(contract);
+  await testDeleteWebsite(contract);
+  console.log('Finished test\n');
 }
 
 main();
