0747fdb2b3b746c64354b27cfa29c23817f961c8
Thomas Sénéchal <44696378+thomas-senechal@users.noreply.github.com>
2024-10-23 11:34:00 +0200

    97 update server with new sc (#127)
    * Add new cache library that matches our needs for website storage
    
    * Update pkg/website for new SC
    
    * Partially updated pkg/webmanager for new SC
    
    * Update getWebsiteResource to handle missing .html extension
    
    * Fix logger warning message format in localHandler
    
    * Dumb fixes for cli to compile
    
    * Update keys for new SC
    
    * Update and refactor storage key

--- server/pkg/webmanager/manager.go
+++ server/pkg/webmanager/manager.go
@@ -2,13 +2,12 @@ package webmanager
 
 import (
 	"fmt"
-	"os"
+	"strings"
 	"time"
 
-	"github.com/massalabs/deweb-server/int/zipper"
+	"github.com/massalabs/deweb-server/pkg/cache"
 	"github.com/massalabs/deweb-server/pkg/website"
 	msConfig "github.com/massalabs/station/int/config"
-	"github.com/massalabs/station/pkg/cache"
 	"github.com/massalabs/station/pkg/logger"
 )
 
@@ -18,57 +17,57 @@ const cacheDir = "./websitesCache/"
 func GetWebsiteResource(network *msConfig.NetworkInfos, websiteAddress, resourceName string) ([]byte, bool, error) {
 	logger.Debugf("Getting website %s resource %s", websiteAddress, resourceName)
 
-	websiteBytes, err := RequestWebsite(websiteAddress, network)
+	content, err := RequestFile(websiteAddress, network, resourceName)
 	if err != nil {
-		return nil, false, fmt.Errorf("failed to get website: %w", err)
+		if strings.Contains(err.Error(), "not found") {
+			return nil, true, fmt.Errorf("failed to get file %s from website %s: %w", resourceName, websiteAddress, err)
+		}
+
+		return nil, false, fmt.Errorf("failed to get file %s from website %s: %w", resourceName, websiteAddress, err)
 	}
 
-	logger.Debugf("Website %s successfully retrieved", websiteAddress)
-
-	content, err := zipper.ReadFileFromZip(websiteBytes, resourceName)
-	if err != nil {
-		logger.Warnf("Filenot found: %b", zipper.IsNotFoundError(err, resourceName))
-		return nil, zipper.IsNotFoundError(err, resourceName), fmt.Errorf("failed to get file from zip: %w", err)
-	}
+	logger.Debugf("Resource %s from %s successfully retrieved", resourceName, websiteAddress)
 
 	return content, false, nil
 }
 
-// RequestWebsite fetches a website and caches it, or retrieves it from the cache if already present.
-func RequestWebsite(scAddress string, networkInfo *msConfig.NetworkInfos) ([]byte, error) {
-	cache := new(cache.Cache)
-	fileName := fmt.Sprintf("website_%s.zip", scAddress)
-
-	lastUpdatedUint, err := website.GetLastUpdateTimestamp(networkInfo, scAddress)
+// RequestFile fetches a website and caches it, or retrieves it from the cache if already present.
+func RequestFile(scAddress string, networkInfo *msConfig.NetworkInfos, resourceName string) ([]byte, error) {
+	cache, err := cache.NewCache(cacheDir)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get last update timestamp: %w", err)
+		return nil, fmt.Errorf("failed to create cache: %w", err)
 	}
 
-	lastUpdated := time.UnixMilli(int64(lastUpdatedUint))
+	// TODO: LastUpdateTimestamp is not yet implemented in the new SC
+	// lastUpdatedUint, err := website.GetLastUpdateTimestamp(networkInfo, scAddress)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("failed to get last update timestamp: %w", err)
+	// }
 
-	if cache.IsPresent(fileName) {
-		logger.Debugf("Website %s present in cache", scAddress)
+	// lastUpdated := time.UnixMilli(int64(lastUpdatedUint))
 
-		isOutdated, err := isFileOutdated(fileName, lastUpdated)
+	if cache.IsPresent(scAddress, resourceName) {
+		logger.Debugf("Resource %s from %s present in cache", resourceName, scAddress)
+
+		// isOutdated, err := isFileOutdated(resourceName, lastUpdated)
+		// if err != nil {
+		// 	return nil, fmt.Errorf("failed to check if file is outdated: %w", err)
+		// }
+
+		// if !isOutdated {
+		content, err := cache.Read(scAddress, resourceName)
 		if err != nil {
-			return nil, fmt.Errorf("failed to check if file is outdated: %w", err)
+			return nil, fmt.Errorf("failed to read cached resource %s from %s: %w", resourceName, scAddress, err)
 		}
 
-		if !isOutdated {
-			content, err := cache.Read(fileName)
-			if err != nil {
-				return nil, fmt.Errorf("failed to read cached website: %w", err)
-			}
-
-			return content, nil
-		}
-
-		logger.Warnf("website %s is outdated, fetching...", fileName)
+		return content, nil
+		// }
+		// logger.Warnf("website %s is outdated, fetching...", resourceName)
 	}
 
 	logger.Debugf("Website %s not found in cache or not up to date, fetching...", scAddress)
 
-	websiteBytes, err := fetchAndCache(networkInfo, scAddress, cache, fileName)
+	websiteBytes, err := fetchAndCache(networkInfo, scAddress, cache, resourceName)
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch and cache website: %w", err)
 	}
@@ -77,33 +76,35 @@ func RequestWebsite(scAddress string, networkInfo *msConfig.NetworkInfos) ([]byt
 }
 
 // Fetches the website and saves it to the cache.
-func fetchAndCache(networkInfo *msConfig.NetworkInfos, scAddress string, cache *cache.Cache, fileName string) ([]byte, error) {
-	websiteBytes, err := website.Fetch(networkInfo, scAddress)
+func fetchAndCache(networkInfo *msConfig.NetworkInfos, scAddress string, cache *cache.Cache, resourceName string) ([]byte, error) {
+	websiteBytes, err := website.Fetch(networkInfo, scAddress, resourceName)
 	if err != nil {
-		return nil, fmt.Errorf("failed to fetch website: %w", err)
+		if website.IsNotFoundError(err, resourceName) {
+			return nil, fmt.Errorf("website %s not found: %w", scAddress, err)
+		}
+
+		return nil, fmt.Errorf("failed to fetch %s from %s: %w", resourceName, scAddress, err)
 	}
 
-	logger.Debugf("Website fetched successfully with size: %d bytes", len(websiteBytes))
+	logger.Debugf("%s: %s successfully fetched with size: %d bytes", scAddress, resourceName, len(websiteBytes))
 
-	if !zipper.IsValidZip(websiteBytes) {
-		return nil, fmt.Errorf("fetched website is not a valid zip")
+	if err := cache.Save(scAddress, resourceName, websiteBytes); err != nil {
+		return nil, fmt.Errorf("failed to save %s to %s cache: %w", resourceName, scAddress, err)
 	}
 
-	if err := cache.Save(fileName, websiteBytes); err != nil {
-		return nil, fmt.Errorf("failed to save website to cache: %w", err)
-	}
-
-	logger.Infof("Website successfully written to cache at: %s", fileName)
+	logger.Infof("%s: %s successfully written to cache", scAddress, resourceName)
 
 	return websiteBytes, nil
 }
 
 // Compares the last updated timestamp to the file's last modified timestamp.
-func isFileOutdated(fileName string, lastUpdated time.Time) (bool, error) {
-	fi, err := os.Stat(cacheDir + fileName)
+//
+//nolint:unused
+func isFileOutdated(cache *cache.Cache, scAddress string, fileName string, lastUpdated time.Time) (bool, error) {
+	lastModified, err := cache.GetLastModified(scAddress, fileName)
 	if err != nil {
 		return false, fmt.Errorf("failed to get file info: %w", err)
 	}
 
-	return fi.ModTime().Before(lastUpdated), nil
+	return lastModified.Before(lastUpdated), nil
 }
---
+++ server/pkg/website/storagekeys/tags.go
@@ -0,0 +1,12 @@
+package storagekeys
+
+const (
+	FILE_TAG                   = "\x01FILE"
+	FILE_LOCATION_TAG          = "\x02LOCATION"
+	CHUNK_TAG                  = "\x03CHUNK"
+	CHUNK_NB_TAG               = "\x04CHUNK_NB"
+	FILE_METADATA_TAG          = "\x05FM"
+	GLOBAL_METADATA_TAG        = "\x06GM"
+	FILE_METADATA_LOCATION_TAG = "\x07FML"
+	DEWEB_VERSION_TAG          = "\xFFDEWEB_VERSION"
+)
