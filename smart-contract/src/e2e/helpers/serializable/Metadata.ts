396a539624897bd1dbfef9c8ff60e74396d4f927
thomas-senechal <thomas.senechal@pm.me>
2024-10-24 15:03:56 +0200

    Fix metadata ser/deser

--- smart-contract/src/e2e/helpers/serializable/Metadata.ts
+++ smart-contract/src/e2e/helpers/serializable/Metadata.ts
@@ -1,10 +1,16 @@
 import { Args, DeserializedResult, Serializable } from '@massalabs/massa-web3';
 
 export class Metadata implements Serializable<Metadata> {
-  constructor(public key: string = '', public value: string = '') {}
+  constructor(
+    public key: string = '',
+    public value: string = '',
+  ) {}
 
   serialize(): Uint8Array {
-    return new Args().addString(this.key).addString(this.value).serialize();
+    return new Args()
+      .addString(this.key)
+      .addString(this.value)
+      .serialize();
   }
 
   deserialize(data: Uint8Array, offset: number): DeserializedResult<Metadata> {
