68571dd86dbc7706c9c96621bf8f749c75705b51
Phoebe Lartisant <phoebelartisant@proton.me>
2024-10-30 17:31:28 +0100

    add 404 svgs & imgae optimizers

--- server/pages/vite.config.ts
+++ server/pages/vite.config.ts
@@ -7,6 +7,8 @@ import { fileURLToPath } from "url";
 import { OutputBundle, NormalizedOutputOptions } from "rollup";
 
 import { rename, existsSync, readdirSync, rmdirSync } from "fs";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import svgr from "vite-plugin-svgr";
 
 // Helper to get __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
@@ -62,13 +64,28 @@ const MoveHtmlToRootPlugin = (): Plugin => {
 };
 
 export default defineConfig({
-  plugins: [react(), MoveHtmlToRootPlugin()],
+  plugins: [
+    react(),
+    MoveHtmlToRootPlugin(),
+    ViteImageOptimizer(),
+    svgr({
+      svgrOptions: {
+        icon: true, // Replace SVG width and height by a custom value. If value is omitted, it uses 1em in order to make SVG size inherits from text size.
+        svgo: true, // This will enable/disable SVGO optimisation when rendering component
+        replaceAttrValues: {
+          "#fff": "currentColor", // This will replace the fill attribute with currentColor = text color
+        },
+        memo: true, // This will enable/disable memoization
+      },
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, indexHtmlPath),
       },
     },
+
     outDir: resolve(__dirname, defaultDistDir, page),
     assetsDir: ".",
     emptyOutDir: true,
