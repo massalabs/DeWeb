c1405ca664a6ff54b3184ee45028bfb2463664d3
Phoebe Lartisant <phoebelartisant@proton.me>
2024-10-30 17:31:51 +0100

    update todo's

--- server/pages/src/main.tsx
+++ server/pages/src/main.tsx
@@ -2,8 +2,3 @@ import DevHomePage from "./pages/DevHomePage.tsx";
 import { renderApp } from "./utils/RootApp.tsx";
 
 renderApp({ children: <DevHomePage /> });
-
-// import 404 styles in svg
-// import set oops
-// implement themes w/ setting per page ? with option to set theme on search page 
-
