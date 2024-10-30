2d3930f1d3342e6e3a1f285ef2a9389e5d43df30
Phoebe Lartisant <phoebelartisant@proton.me>
2024-10-30 17:00:55 +0100

    refacto colors presets

--- server/pages/src/colors/preset.js
+++ server/pages/src/colors/preset.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import plugin from "tailwindcss/plugin";
-import themeConfig from "./themeConfig.js";
+import { colors } from "./colors";
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const { createThemes } = require("tw-colors");
@@ -10,11 +10,92 @@ module.exports = {
   theme: {
     fontFamily: {
       plex: ["IBM Plex Mono", "monospace"],
-      clock: ["E1234", "sans-serif"]
-    }
+      clock: ["E1234", "sans-serif"],
+    },
   },
   plugins: [
-    createThemes(themeConfig),
+    createThemes({
+      neon_green_forest: {
+        primary: colors.greenNeon,
+        secondary: colors.darkForestGreen,
+      },
+      bright_blue_navy: {
+        primary: colors.blueBright,
+        secondary: colors.deepNavyBlue,
+      },
+      sunny_magenta: {
+        primary: colors.yellowBright,
+        secondary: colors.magentaBright,
+      },
+      magenta_sunny: {
+        primary: colors.magentaBright,
+        secondary: colors.yellowBright,
+      },
+      olive_sunshine: {
+        primary: colors.darkOliveGreen,
+        secondary: colors.yellowBright,
+      },
+      magenta_midnight: {
+        primary: colors.magentaBright,
+        secondary: colors.darkPurple,
+      },
+      red_cyan: {
+        primary: colors.redBright,
+        secondary: colors.cyanBright,
+      },
+      cyan_red: {
+        primary: colors.cyanBright,
+        secondary: colors.redBright,
+      },
+      maroon_red: {
+        primary: colors.darkMaroon,
+        secondary: colors.redBright,
+      },
+      teal_cyan: {
+        primary: colors.darkTeal,
+        secondary: colors.cyanBright,
+      },
+      sunset_purple: {
+        primary: colors.orangeBright,
+        secondary: colors.purpleRoyal,
+      },
+      purple_sunset: {
+        primary: colors.purpleRoyal,
+        secondary: colors.orangeBright,
+      },
+      brown_sunset: {
+        primary: colors.darkBrown,
+        secondary: colors.orangeBright,
+      },
+      midnight_purple: {
+        primary: colors.midnightBlue,
+        secondary: colors.purpleRoyal,
+      },
+      charcoal_neon_pink: {
+        primary: colors.charcoal,
+        secondary: colors.pinkNeon,
+      },
+      neon_pink_charcoal: {
+        primary: colors.pinkNeon,
+        secondary: colors.charcoal,
+      },
+      plum_neon_pink: {
+        primary: colors.plum,
+        secondary: colors.pinkNeon,
+      },
+      pastel_neon_pink: {
+        primary: colors.pastelPink,
+        secondary: colors.pinkNeon,
+      },
+      light_gray_blue: {
+        primary: colors.CelestialMist,
+        secondary: colors.DeepBlue,
+      },
+      deep_blue_gray: {
+        primary: colors.DeepBlue,
+        secondary: colors.CelestialMist,
+      },
+    }),
     plugin(function ({ addComponents, theme }) {
       addComponents({
         ".paragraph-lg": {
@@ -23,7 +104,7 @@ module.exports = {
           fontFamily: theme("fontFamily.plex"),
           lineHeight: "42px",
           fontStyle: "normal",
-          userSelect: "none"
+          userSelect: "none",
         },
         ".paragraph-light-lg": {
           fontSize: "28px",
@@ -31,7 +112,7 @@ module.exports = {
           fontFamily: theme("fontFamily.plex"),
           lineHeight: "42px",
           fontStyle: "normal",
-          userSelect: "none"
+          userSelect: "none",
         },
         ".paragraph-italic-lg": {
           fontSize: "30px",
@@ -39,7 +120,7 @@ module.exports = {
           fontFamily: theme("fontFamily.plex"),
           lineHeight: "42px",
           fontStyle: "italic",
-          userSelect: "none"
+          userSelect: "none",
         },
         ".paragraph-italic-light-lg": {
           fontSize: "30px",
@@ -47,7 +128,7 @@ module.exports = {
           fontFamily: theme("fontFamily.plex"),
           lineHeight: "42px",
           fontStyle: "italic",
-          userSelect: "none"
+          userSelect: "none",
         },
         ".title-lg": {
           fontSize: "46px",
@@ -55,7 +136,7 @@ module.exports = {
           fontFamily: theme("fontFamily.plex"),
           lineHeight: "62px",
           fontStyle: "semi-bold",
-          userSelect: "none"
+          userSelect: "none",
         },
 
         // medium
@@ -65,7 +146,7 @@ module.exports = {
           fontFamily: theme("fontFamily.plex"),
           lineHeight: "36px",
           fontStyle: "normal",
-          userSelect: "none"
+          userSelect: "none",
         },
         ".paragraph-light-md": {
           fontSize: "22px",
@@ -73,7 +154,7 @@ module.exports = {
           fontFamily: theme("fontFamily.plex"),
           lineHeight: "36px",
           fontStyle: "normal",
-          userSelect: "none"
+          userSelect: "none",
         },
         ".paragraph-italic-md": {
           fontSize: "24px",
@@ -81,7 +162,7 @@ module.exports = {
           fontFamily: theme("fontFamily.plex"),
           lineHeight: "36px",
           fontStyle: "italic",
-          userSelect: "none"
+          userSelect: "none",
         },
         ".paragraph-italic-light-md": {
           fontSize: "24px",
@@ -89,7 +170,7 @@ module.exports = {
           fontFamily: theme("fontFamily.plex"),
           lineHeight: "36px",
           fontStyle: "italic",
-          userSelect: "none"
+          userSelect: "none",
         },
         ".title-md": {
           fontSize: "36px",
@@ -97,7 +178,7 @@ module.exports = {
           fontFamily: theme("fontFamily.plex"),
           lineHeight: "52px",
           fontStyle: "semi-bold",
-          userSelect: "none"
+          userSelect: "none",
         },
         // mobile
         ".paragraph-sm": {
@@ -106,7 +187,7 @@ module.exports = {
           fontFamily: theme("fontFamily.plex"),
           lineHeight: "32px",
           fontStyle: "normal",
-          userSelect: "none"
+          userSelect: "none",
         },
         ".paragraph-light-sm": {
           fontSize: "14px",
@@ -114,7 +195,7 @@ module.exports = {
           fontFamily: theme("fontFamily.plex"),
           lineHeight: "32px",
           fontStyle: "normal",
-          userSelect: "none"
+          userSelect: "none",
         },
         ".paragraph-italic-sm": {
           fontSize: "16px",
@@ -122,7 +203,7 @@ module.exports = {
           fontFamily: theme("fontFamily.plex"),
           lineHeight: "32px",
           fontStyle: "italic",
-          userSelect: "none"
+          userSelect: "none",
         },
         ".paragraph-italic-light-sm": {
           fontSize: "16px",
@@ -130,7 +211,7 @@ module.exports = {
           fontFamily: theme("fontFamily.plex"),
           lineHeight: "32px",
           fontStyle: "italic",
-          userSelect: "none"
+          userSelect: "none",
         },
         ".title-sm": {
           fontSize: "28px",
@@ -138,7 +219,7 @@ module.exports = {
           fontFamily: theme("fontFamily.plex"),
           lineHeight: "46px",
           fontStyle: "semi-bold",
-          userSelect: "none"
+          userSelect: "none",
         },
 
         // window
@@ -152,11 +233,11 @@ module.exports = {
           gap: "32px",
           overflow: "scroll",
           padding: "24px 4px",
-          overflowY: "scroll"
+          overflowY: "scroll",
         },
 
         ".pan-action": {
-          touchAction: "pan-x"
+          touchAction: "pan-x",
         },
 
         // Clock
@@ -164,19 +245,19 @@ module.exports = {
           fontSize: "60px",
           fontWeight: "500",
           fontFamily: theme("fontFamily.clock"),
-          userSelect: "none"
+          userSelect: "none",
         },
         ".clock-md": {
           fontSize: "48px",
           fontWeight: "500",
           fontFamily: theme("fontFamily.clock"),
-          userSelect: "none"
+          userSelect: "none",
         },
         ".clock-sm": {
           fontSize: "36px",
           fontWeight: "500",
           fontFamily: theme("fontFamily.clock"),
-          userSelect: "none"
+          userSelect: "none",
         },
 
         // Custom Button
@@ -189,7 +270,7 @@ module.exports = {
           borderRadius: "8px",
           color: theme("colors.secondary"),
           borderColor: theme("colors.secondary"),
-          backgroundColor: theme("colors.primary")
+          backgroundColor: theme("colors.primary"),
         },
         ".custom-button-secondary": {
           display: "flex",
@@ -200,9 +281,9 @@ module.exports = {
           borderRadius: "8px",
           color: theme("colors.primary"),
           borderColor: theme("colors.primary"),
-          backgroundColor: theme("colors.secondary")
-        }
+          backgroundColor: theme("colors.secondary"),
+        },
       });
-    })
-  ]
+    }),
+  ],
 };
