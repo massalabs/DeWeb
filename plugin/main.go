ab46c1c2f6d13e75c50738e9b56ad78bb95790f7
thomas-senechal <thomas.senechal@pm.me>
2025-01-07 11:28:30 +0100

    Add initial plugin implementation

---
+++ plugin/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/massalabs/deweb-server/int/api"
+	"github.com/massalabs/deweb-server/int/api/config"
+	pkgConfig "github.com/massalabs/deweb-server/pkg/config"
+	"github.com/massalabs/station/pkg/logger"
+)
+
+const directoryName = "station-deweb-plugin"
+
+func main() {
+	pluginDir, err := PluginDir()
+	if err != nil {
+		log.Fatalf("failed to get plugin directory: %v", err)
+	}
+
+	logPath := filepath.Join(pluginDir, "./deweb-plugin.log")
+
+	err = logger.InitializeGlobal(logPath)
+	if err != nil {
+		log.Fatalf("failed to initialize logger: %v", err)
+	}
+
+	conf := config.ServerConfig{
+		APIPort:      0,
+		Domain:       "localhost",
+		NetworkInfos: pkgConfig.NewNetworkConfig("https://buildnet.massa.net/api/v2"),
+		CacheDir:     filepath.Join(pluginDir, "websiteCache"),
+	}
+
+	api := api.NewPluginAPI(&conf)
+	api.Start()
+}
+
+func PluginDir() (string, error) {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return "", fmt.Errorf("getting user config directory: %w", err)
+	}
+
+	path := filepath.Join(configDir, directoryName)
+
+	// create the directory if it doesn't exist
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err = os.MkdirAll(path, os.ModePerm)
+		if err != nil {
+			return "", fmt.Errorf("creating account directory '%s': %w", path, err)
+		}
+	}
+
+	return path, nil
+}
