b02acd9fae603fb8d933a1c304ce728cf47a2932
Andrei Fleiser <fleandrei@yahoo.com>
2025-03-11 16:43:34 +0100

    fix lint prettier issue

--- cli/tests/fileInit.spec.ts
+++ cli/tests/fileInit.spec.ts
@@ -6,12 +6,29 @@ import { Metadata } from '../src/lib/website/models/Metadata'
 
 describe('createBatches', () => {
   it('should create batches with the correct number of elements', () => {
-    const files = Array.from({ length: batchSize * 2 + 1 }, (_, i) => new FileInit(`file${i}`))
-    const filesToDelete = Array.from({ length: batchSize * 2 - 1 }, (_, i) => new FileDelete(`hash${i}`))
-    const metadatas = Array.from({ length: batchSize }, (_, i) => new Metadata(`key${i}`, `value${i}`))
-    const metadatasToDelete = Array.from({ length: 2 }, (_, i) => new Metadata(`key${i}`, `value${i}`))
+    const files = Array.from(
+      { length: batchSize * 2 + 1 },
+      (_, i) => new FileInit(`file${i}`)
+    )
+    const filesToDelete = Array.from(
+      { length: batchSize * 2 - 1 },
+      (_, i) => new FileDelete(`hash${i}`)
+    )
+    const metadatas = Array.from(
+      { length: batchSize },
+      (_, i) => new Metadata(`key${i}`, `value${i}`)
+    )
+    const metadatasToDelete = Array.from(
+      { length: 2 },
+      (_, i) => new Metadata(`key${i}`, `value${i}`)
+    )
 
-    const batches = createBatches(files, filesToDelete, metadatas, metadatasToDelete)
+    const batches = createBatches(
+      files,
+      filesToDelete,
+      metadatas,
+      metadatasToDelete
+    )
 
     expect(batches).toHaveLength(3)
     expect(batches[0].fileInits).toHaveLength(batchSize)
@@ -31,18 +48,34 @@ describe('createBatches', () => {
     const metadatas: Metadata[] = []
     const metadatasToDelete: Metadata[] = []
 
-    const batches = createBatches(files, filesToDelete, metadatas, metadatasToDelete)
+    const batches = createBatches(
+      files,
+      filesToDelete,
+      metadatas,
+      metadatasToDelete
+    )
 
     expect(batches).toHaveLength(0)
   })
 
   it('should handle inputs with same lengths', () => {
     const files = Array.from({ length: 2 }, (_, i) => new FileInit(`file${i}`))
-    const filesToDelete = Array.from({ length: 2 }, (_, i) => new FileDelete(`hash${i}`))
-    const metadatas = Array.from({ length: 2 }, (_, i) => new Metadata())
-    const metadatasToDelete = Array.from({ length: 2 }, (_, i) => new Metadata(`key${i}`))
+    const filesToDelete = Array.from(
+      { length: 2 },
+      (_, i) => new FileDelete(`hash${i}`)
+    )
+    const metadatas = Array.from({ length: 2 }, () => new Metadata())
+    const metadatasToDelete = Array.from(
+      { length: 2 },
+      (_, i) => new Metadata(`key${i}`)
+    )
 
-    const batches = createBatches(files, filesToDelete, metadatas, metadatasToDelete)
+    const batches = createBatches(
+      files,
+      filesToDelete,
+      metadatas,
+      metadatasToDelete
+    )
 
     expect(batches).toHaveLength(1)
     expect(batches[0].fileInits).toHaveLength(2)
